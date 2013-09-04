e647c29e4a0158fe641db349c6a25866d8d336a0
Michael MacCallum <maccdevteam@gmail.com>
2013-09-04 14:27:36 -0400

    Initial Commit

---
+++ RotatingImageView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RotatingImageView
+//
+//  Created by Mick on 9/4/13.
+//  Copyright (c) 2013 HappTech Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
