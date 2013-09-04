f273758290ef4e240bcbb6f650a22f7c7e023276
Michael MacCallum <maccdevteam@gmail.com>
2013-09-04 15:37:57 -0400

    Initial commit everything should work.
    This project contains an example of how to a UIPinchGestureRecognizer,
    a UIRotationGestureRecognizer, and a UIPanGestureRecognizer to a
    UIImageView. This project was created to answer a Stack Overflow
    question.

--- RotatingImageView/ViewController.m
+++ RotatingImageView/ViewController.m
@@ -1,14 +1,8 @@
-//
-//  ViewController.m
-//  RotatingImageView
-//
-//  Created by Mick on 9/4/13.
-//  Copyright (c) 2013 HappTech Development. All rights reserved.
-//
+
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () 
 
 @end
 
@@ -17,13 +11,77 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    // set up the image view
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"someImage"]];
+    [imageView setBounds:CGRectMake(0.0, 0.0, 120.0, 120.0)];
+    [imageView setCenter:self.view.center];
+    [imageView setUserInteractionEnabled:YES]; // <--- This is very important
+
+    // create and configure the pinch gesture
+    UIPinchGestureRecognizer *pinchGestureRecognizer = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(pinchGestureDetected:)];
+    [pinchGestureRecognizer setDelegate:self];
+    [imageView addGestureRecognizer:pinchGestureRecognizer];
+
+    // create and configure the rotation gesture
+    UIRotationGestureRecognizer *rotationGestureRecognizer = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(rotationGestureDetected:)];
+    [rotationGestureRecognizer setDelegate:self];
+    [imageView addGestureRecognizer:rotationGestureRecognizer];
+
+    // creat and configure the pan gesture
+    UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGestureDetected:)];
+    [panGestureRecognizer setDelegate:self];
+    [imageView addGestureRecognizer:panGestureRecognizer];
+
+
+    [self.view addSubview:imageView]; // add the image view as a subview of the view controllers view
+}
+
+
+- (void)pinchGestureDetected:(UIPinchGestureRecognizer *)recognizer
+{
+    UIGestureRecognizerState state = recognizer.state;
+
+    if (state == UIGestureRecognizerStateBegan || state == UIGestureRecognizerStateChanged)
+    {
+        CGFloat scale = [recognizer scale];
+        [recognizer.view setTransform:CGAffineTransformScale(recognizer.view.transform, scale, scale)];
+        [recognizer setScale:1.0];
+    }
+}
+
+- (void)rotationGestureDetected:(UIRotationGestureRecognizer *)recognizer
+{
+    UIGestureRecognizerState state = recognizer.state;
+
+    if (state == UIGestureRecognizerStateBegan || state == UIGestureRecognizerStateChanged)
+    {
+        CGFloat rotation = [recognizer rotation];
+        [recognizer.view setTransform:CGAffineTransformRotate(recognizer.view.transform, rotation)];
+        [recognizer setRotation:0];
+    }
+}
+
+- (void)panGestureDetected:(UIPanGestureRecognizer *)recognizer
+{
+    UIGestureRecognizerState state = recognizer.state;
+
+    if (state == UIGestureRecognizerStateBegan || state == UIGestureRecognizerStateChanged)
+    {
+        CGPoint translation = [recognizer translationInView:recognizer.view];
+        [recognizer.view setTransform:CGAffineTransformTranslate(recognizer.view.transform, translation.x, translation.y)];
+        [recognizer setTranslation:CGPointZero inView:recognizer.view];
+    }
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
+{
+    return YES;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
